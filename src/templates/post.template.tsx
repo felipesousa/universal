13f28f782dab81e78751ecb285a50738abe3913e
Felipe Sousa <felipzbr@gmail.com>
2020-05-18 14:30:10 -0400

    [feature]: finishing template post

--- src/templates/post.template.tsx
+++ src/templates/post.template.tsx
@@ -21,6 +21,7 @@ export const query = graphql`
       }
       html
       timeToRead
+      excerpt
     }
   }
 `;
@@ -36,8 +37,8 @@ const lang = {
   },
 };
 
-const PostTemplate = ({ data: { markdownRemark: post } }) => {
-  let { language } = useContext(LanguageContext);
+const PostTemplate = ({ data: { markdownRemark: post }, location }) => {
+  let { language, theme } = useContext(LanguageContext);
 
   useEffect(() => {
     if (post.frontmatter.lang !== language) {
@@ -49,16 +50,29 @@ const PostTemplate = ({ data: { markdownRemark: post } }) => {
     <Layout>
       <SEO title={post.frontmatter.title} />
       <SectionTitle line={false}>{post.frontmatter.title}</SectionTitle>
-      <PostDetails>
-        <span>
-          {lang["published"][language]}
-          {post.frontmatter.date}.
-        </span>
 
-        <span>
-          {lang["timeToRead"][language]}
-          {post.timeToRead} min.
-        </span>
+      <PostDetails>
+        <div>
+          <span>
+            {lang["published"][language]}
+            {post.frontmatter.date}.
+          </span>
+
+          <span>
+            {lang["timeToRead"][language]}
+            {post.timeToRead} min.
+          </span>
+        </div>
+
+        <a
+          href={`https://twitter.com/intent/tweet?url=${location.href}&text=${post.excerpt}`}
+          target="_blank"
+        >
+          Share on Twitter
+          <img
+            src={`/images/twitter-${theme == "dark" ? "light" : "dark"}.svg`}
+          />
+        </a>
       </PostDetails>
 
       <Content dangerouslySetInnerHTML={{ __html: post.html }}></Content>
@@ -83,15 +97,45 @@ const Content = styled.article`
 `;
 
 const PostDetails = styled.section`
-  font-family: "Slab Bold";
-  font-size: 1.2rem;
+  border-bottom: 5px solid var(--gray);
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
   align-items: center;
-  margin-bottom: 40px;
+  font-family: "Slab Regular";
+  margin-top: 40px;
+  padding-bottom: 10px;
+  margin-bottom: 60px;
+  font-size: 1.2rem;
+
+  div {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: auto;
+  }
+
+  a {
+    display: flex;
+    color: var(--black);
+    text-transform: capitalize;
+    margin-bottom: 10px;
+
+    img {
+      width: 27px;
+      height: 27px;
+      margin: 0;
+      margin-left: 10px;
+    }
+
+    @media screen and (max-width: 768px) {
+      display: flex;
+    }
+  }
 
   span {
     margin: 0;
+    margin-bottom: 10px;
 
     &:first-of-type {
       margin-right: 10px;
