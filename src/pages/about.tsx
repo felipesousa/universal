29bde19de30c9f8d7b15ecc9e197b85b95c4a6b2
Felipe Sousa <felipzbr@gmail.com>
2020-08-21 10:25:05 -0300

    [content]: about me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -19,9 +19,8 @@ const AboutPage = () => {
           <Content>
             <h1>👋🏻</h1>
             <p>
-              22, Brazilian born in Fortaleza/Ceará. Living in Santiago/Chile
-              since July 2019 and working with Front End Engineer at
-              @concretelatinoamerica.
+              Hi, my name is Felipe, front-end engineer at
+              @concretelatinoamerica and living in Santiago - Chile.
             </p>
             <p>
               First of all, a big thank you for visiting and dedicating your
@@ -29,43 +28,22 @@ const AboutPage = () => {
             </p>
             <h2>How it all began?</h2>
             <p>
-              My first contact with programming happened when I was ~14y.o.
-              (2012) while browsing Google, I remember I met Blogger project,
-              which was a blog creator, I remember well what was the feeling of
-              creating the first page, seeing the first plugins - from simple
-              banners to online people counters 🤯, however simple as they were,
-              the feeling of something working is still today, {""}{" "}
-              <strong style={{ textDecoration: "none" }}> awesome </strong> -
-              see how a "heap" of colored letters becomes something visually
-              interactive made me super excited, it caught my attention in hour!
+              My first contact with programming happened when I was around 14
+              years old while browsing Google, I remember that I got to know the
+              Blogger project, I remember how it felt to create the first page,
+              see the first plugins - from simple banners, some scripts copied
+              from the internet, etc., for more simple as that was, it caught my
+              attention at the time! Oftentimes studied through videos and some
+              forums that exist / existed inside the internet, interactions were
+              simply with HTML and a rare and unknown CSS.
             </p>
             <p>
-              I studied on YouTube and some forums that existed within the
-              interactions, they were simply with HTML and a rare and unknown
-              CSS.
-            </p>
-            <p>
-              From 15 to 17 years old (2013-2015), I entered a public school of
-              technical education that was where I studied in a more theoretical
-              way, initial concepts about programming with PHP, Java and a
-              little bit of JavaScript, it was certainly a fantastic place that
-              contributed for my focus on technical studies. In the meantime, I
-              involved directly with the community with the 1st edition of Front
-              In Fortaleza, which in years later I would come to{" "}
-              <a
-                href={`/talks/${language}/front-in-fortaleza-2016`}
-                style={{ textDecoration: "underline" }}
-              >
-                speak
-              </a>{" "}
-              and also{" "}
-              <a
-                href={`/talks/${language}/front-in-fortaleza-2018`}
-                style={{ textDecoration: "underline" }}
-              >
-                be part of organization
-              </a>{" "}
-              at 2018.
+              Over the months I started to study programming with PHP, Java and
+              a little bit of JavaScript, over time I was actively participating
+              local and regional communities. One of the most important events
+              at the beginning it was Front In Fortaleza, according to each
+              edition I went getting more involved with other communities,
+              participating in events and gradually increasing my networking.
             </p>
             <img src="/images/about.jpg" alt="about banner image" />
             📸: <i>DevFest Santiago 2019, Chile.</i>
@@ -80,53 +58,32 @@ const AboutPage = () => {
                 target="_blank"
                 style={{ textDecoration: "underline" }}
               >
-                I have been through some companies, projects and technologies
+                {" "}
+                I have been through some companies, projects, and technologies
                 interesting.
-              </a>{" "}
-              Always clear focusing on delivery, performance and quality.
+              </a>
             </p>
             <p>
-              Within the almost ~ 5 years of career, I have worked in great part
-              as front end. From responsive pages to applications with React
-              Native, migrating applications to Progressive Web Apps, or
-              creating some tools for NodeJS, etc., always trying to follow good
-              practices and delivering value as core values.
+              Within the past 5 years I have worked largely as a front-end. From
+              responsive pages, hybrid, native applications with React Native,
+              or creating some tools for NodeJS, etc., many of them for
+              companies in Brazil, USA and Chile.
             </p>
             <p>
               Today, I have studied and worked a lot with React/Angular, JS/TS,
-              Data Manager (Redux / Flux), CSS-in-JS, FP, RXJS, webpack, tooling
-              and the entire <i> new </i> JavaScript ecosystem. I have done
-              recent experiments with Rust and Python, I learn a lot by watching
-              other programming languages and seeing code from others
-              programmers, mainly on GitHub.
+              Data Manager (Redux/Flux), CSS-in-JS, FP, RXJS, webpack, tooling
+              and the entire <i>new</i> JavaScript ecosystem. Have recent
+              experiments with Rust and Python, I learn a lot seeing other
+              programming languages and seeing code from others programmers,
+              mainly on GitHub.
             </p>
             <h2>Community & Open-Source</h2>
             <p>
-              Front In Fortaleza was just the starter of what came to be be my
+              Front In Fortaleza was just the kick-off of what came to be my
               relationship with community (s). At the end of 2015 I received a
-              invitation{" "}
-              <a
-                href="https://ythecombinator.space"
-                target="_blank"
-                style={{ textDecoration: "underline" }}
-              >
-                @ythecombinator
-              </a>{" "}
-              to present{" "}
-              <a
-                href={`talks/${language}/1st-nodejs-ce-meetup`}
-                style={{ textDecoration: "underline" }}
-              >
-                my first lightining-talk
-              </a>
-              . It was a positive experience and weeks later I receive the
-              oportunity to present a sencond call at 2nd Ionic Meetup
-              Fortaleza.{" "}
-              <a href="/talks" style={{ textDecoration: "underline" }}>
-                You can see almost every presentation I've ever had opportunity
-                to share in some events I attended
-              </a>{" "}
-              and see some of my contribution in communities.
+              invitation to my first lightining-talk. It was an experience
+              positive and that has become increasingly part of my learning
+              process.
             </p>
             <img src="/images/devfest.jpg" alt="speaking banner image" />
             📸: <i>DevFest Nordeste 2016, Maceió/Brazil.</i>
@@ -150,32 +107,20 @@ const AboutPage = () => {
               Open-source and community for me are directly linked, since I have
               always focused on open-source as a faster alternative to apply
               knowledge, learn from professionals, share experiments and support
-              even in a simple way for projects that like.
+              even in a simple way for projects that I like.
             </p>
             <p>
-              The idea of creating the blog and feeding content is precisely a
-              way to contribute a portion to the community. Besides being a
-              great personal guide. Thanks to this type of initiative, I met
-              some fantastic places to go to events and also to do great
-              friends.
+              The idea behind this blog is precisely a way to contribute with a
+              share for the community. In addition to being a great personal
+              guide.
             </p>
             <p>
-              Thanks for reading, I hope you like it! Here I will share more
-              details about my <a href="/posts">studies</a>,{" "}
-              <a href="https://github.com/felipesousa" target="_blank">
-                projects
-              </a>
-              , <a href="/talks">presentations</a> and{" "}
-              <a href="https://linkedin.com/in/luisfelipesousa" target="_blank">
-                career.
-              </a>
+              I hope you like it, here is a coffee collective, a lot of music
+              and hours typing -<i> these are the things I like to do most </i>{" "}
+              -.
             </p>
-            <p>
-              <a href="mailto:hi@felipesousa.space">
-                if you prefer, you can also send me an email.
-              </a>{" "}
-              📬
-            </p>
+            <br />
+            <p>👨🏻‍💻</p>
           </Content>
         </Layout>
       </>
@@ -198,27 +143,25 @@ const AboutPage = () => {
               Antes de tudo, um grande obrigado por visitar e dedicar seu tempo
               a conhecer um pouco da minha história.{" "}
             </p>
-            {/* <img src="/images/about2.jpg" alt="about2 banner image" />
-            📸:<i> Company Office 2019, Chile.</i> */}
             <h2>Como tudo começou?</h2>
             <p>
               Meu primeiro contato com programação aconteceu quando eu tinha por
               volta de 14 anos enquanto navegava pelo Google, me lembro que
-              conheci o projeto Blogger que era um blog creator, lembro bem qual
-              foi a sensação de criar a primeira página, ver os primeiros
-              plugins - desde simples banners, alguns scripts copiados da
-              internet, etc., por mais simples que fossem, aquilo me chamou
-              atenção na hora! Muitas vezes estudava por vídeos e por alguns
-              fóruns que existem/existiam dentro da internet, as interações eram
-              simplesmente com HTML e um raro e desconhecido CSS.
+              conheci o projeto Blogger, lembro bem qual foi a sensação de criar
+              a primeira página, ver os primeiros plugins - desde simples
+              banners, alguns scripts copiados da internet, etc., por mais
+              simples que fossem, aquilo me chamou atenção na hora! Muitas vezes
+              estudava por vídeos e por alguns fóruns que existem/existiam
+              dentro da internet, as interações eram simplesmente com HTML e um
+              raro e desconhecido CSS.
             </p>
             <p>
-              Comecei a estudar programação com PHP, Java e um pouco de
-              JavaScript, com o tempo fui participando ativamente de comunidades
-              locais e regionais. Um dos eventos mais importantes no início foi
-              o Front In Fortaleza, de acordo com cada edição fui me envolvendo
-              mais com outras comunidades, participando em eventos nacionais e
-              aumentando gradualmente meu networking.
+              Com o passar dos meses comecei a estudar programação com PHP, Java
+              e um pouco de JavaScript, com o tempo fui participando ativamente
+              de comunidades locais e regionais. Um dos eventos mais importantes
+              no início foi o Front In Fortaleza, de acordo com cada edição fui
+              me envolvendo mais com outras comunidades, participando em eventos
+              nacionais e aumentando gradualmente meu networking.
             </p>
             <img src="/images/about.jpg" alt="about banner image" />
             📸: <i>DevFest Santiago 2019, Chile.</i>
@@ -258,11 +201,7 @@ const AboutPage = () => {
               a ser minha relação com comunidade(s). No final de 2015 recebi um
               convite para minha primeira lightining-talk. Foi uma experiência
               positiva e que veio se tornando cada vez mais parte do meu
-              processo de aprendizagem. Na sessão de
-              <a href="/talks" style={{ textDecoration: "underline" }}>
-                palestras
-              </a>{" "}
-              têm um pouco do que já apresentei.
+              processo de aprendizagem.
             </p>
             <img src="/images/devfest.jpg" alt="speaking banner image" />
             📸: <i>DevFest Nordeste 2016, Maceió/Brasil.</i>
@@ -290,16 +229,16 @@ const AboutPage = () => {
               gosto.
             </p>
             <p>
-              A ideia de criar o blog e alimentar com conteúdo é justamente uma
-              forma de contribuir com uma parcela para a comunidade. Além de ser
-              um ótimo guia pessoal. Graças a esse tipo de iniciativa, conheci
-              alguns fantásticos lugares para ir a eventos e também a fazer
-              grandes amigos.
+              A ideia detrás desse blog é justamente uma forma de contribuir com
+              uma parcela para a comunidade. Além de ser um ótimo guia pessoal.
             </p>
             <p>
-              Espero que gostem, aqui é um coletivo de café e horas digitando -
-              essas são as coisas que mais gosto de fazer -.
+              Espero que gostem, aqui é um coletivo de café, muita música e
+              horas digitando -{" "}
+              <i>essas são as coisas que mais gosto de fazer</i> -.
             </p>
+            <br />
+            <p>👨🏻‍💻</p>
           </Content>
         </Layout>
       </>
